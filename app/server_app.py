71e66c7bf116f5f1b24286ff79ecb8408af4d4a0
trydonot <80401479+trydonot@users.noreply.github.com>
2022-12-20 00:08:50 +0000

    Update server_app.py

--- app/server_app.py
+++ app/server_app.py
@@ -2,7 +2,11 @@ from fastapi import FastAPI
 import psycopg2
 
 connection = psycopg2.connect(user='airflow', password='airflow', host='host.docker.internal', port='5432', database='postgres')
-GLOBAL_AVG = "select names.name as _name, AVG(av_rating) as score, Count(names.name) as ntitles, SUM(runtime_minutes) " \
+
+# sql query to list of [category] ordered by their rating score, which we will calculate by the
+# average of movie ratings, with Name,Score, Number of Titles as Principal, total number of runtime minutes
+
+get_list_act = "select names.name as _name, AVG(av_rating) as score, Count(names.name) as ntitles, SUM(runtime_minutes) " \
              "as truntime from names inner join titles_names on name_id = names.id join titles on title_id = " \
              "titles.id where category = {} group by _name, category; "
 app = FastAPI()
@@ -13,9 +17,9 @@ def get_list_db(category: str):
     category = "'" + category + "'"
     with connection:
         with connection.cursor() as cursor:
-            comand = GLOBAL_AVG.format(category)
+            comand = get_list_act.format(category)
             cursor.execute(comand)
             average = cursor.fetchall()
             print(average)
-    return {"average": average}
+    return {"list": average}
 
